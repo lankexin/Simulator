8260e8ce6e2c6b36c0fe2f5a9c63c455e2ccfb5e
Kexin Lan <kexinlan@KexindeMacBook-Pro.local>
2019-08-22 21:42:13 +0800

    task extraction

--- Simulator/src/realtime/TaskExtraction.java
+++ Simulator/src/realtime/TaskExtraction.java
@@ -1,24 +1,58 @@
 package realtime;
 
-import lmf.Channel;
-import lmf.Component;
-import lmf.Task;
+import lmf.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TaskExtraction {
 
-    public TaskExtraction() {
-
-    }
-
     public Map<String, Task> taskExtraction(Map<String, Component> componentMap, List<Channel> channelList) {
-        Map<String, Task> taskMap = new HashMap<>();
+        Map<String, Task> taskMap = new LinkedHashMap<>();
+
+        //Map<String, PathNode> pathNodeMap = new HashMap<>();
+        //getSystemPathNode(channelList, componentMap, pathNodeMap);
+
 
         return taskMap;
     }
+
+//    private static void getSystemPathNode(List<Channel> channelList,
+//                                          Map<String, Component> componentMap,
+//                                          Map<String, PathNode> pathNodeList) {
+//        //System.out.println(channelList.size());
+//        for (Channel channel : channelList) {
+//            String sourceId = channel.getAttr("source");
+//            for (String componentKey : componentMap.keySet()) {
+//                if (componentMap.get(componentKey).getLinkpointMap().get(sourceId) != null) {
+//                    sourceId = componentMap.get(componentKey).getAttr("id");
+//                }
+//            }
+//            String destId = channel.getAttr("dest");
+//            for (String componentKey : componentMap.keySet()) {
+//                if (componentMap.get(componentKey).getLinkpointMap().get(destId) != null) {
+//                    destId = componentMap.get(componentKey).getAttr("id");
+//                }
+//            }
+//            //System.out.println("source id is " + sourceId + " dest id is " + destId);
+//            if (componentMap.get(destId) == null || componentMap.get(sourceId) == null) continue;
+//            if (pathNodeList.get(destId) != null) pathNodeList.get(destId).setIsFirst(false);
+//            else {
+//                PathNode newDestNode = new PathNode(destId, componentMap.get(destId).getAttr("wcet"),
+//                        componentMap.get(destId).getAttr("name"), false);
+//                pathNodeList.put(destId, newDestNode);
+//            }
+//            if (pathNodeList.get(sourceId) != null) {
+//                pathNodeList.get(sourceId).getNextComponents().add(pathNodeList.get(destId));
+//            } else {
+//                PathNode newSourceNode = new PathNode(sourceId, componentMap.get(sourceId).getAttr("wcet"),
+//                        componentMap.get(sourceId).getAttr("name"), true);
+//                newSourceNode.getNextComponents().add(pathNodeList.get(destId));
+//                pathNodeList.put(sourceId, newSourceNode);
+//            }
+//
+//        }
+//    }
+
 
     /**
      * 根据组件的连接顺序给任务排序
